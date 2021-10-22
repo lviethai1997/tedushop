f5af17e696affcd384094ffb5fcd59acafde4ae6
DESKTOP-2UPET6P\haile <lviethai1997@gmail.com>
2021-10-22 14:34:10 +0700

    Lam bai 6

--- TeduShop.Model/Models/SupportOnline.cs
+++ TeduShop.Model/Models/SupportOnline.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TeduShop.Model
+namespace TeduShop.Model.Models
 {
     [Table("SupportOnlines")]
     public class SupportOnline
