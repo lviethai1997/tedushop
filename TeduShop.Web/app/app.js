59c1e9ce65d50510d392ad1ea52a268dc697ec90
DESKTOP-2UPET6P\haile <lviethai1997@gmail.com>
2021-11-08 15:39:25 +0700

    update bai 22

--- TeduShop.Web/app/app.js
+++ TeduShop.Web/app/app.js
@@ -1,1 +1,17 @@
-﻿
+﻿/// <reference path="../assets/admin/scripts/angular.js" />
+
+
+(function () {
+    angular.module('tedushop', ['tedushop.products', 'tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/Home/HomeView.html",
+            controller: "HomeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ TeduShop.Web/app/components/Products/products.module.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../../../assets/admin/scripts/angular.js" />
+
+(function () {
+    angular.module('tedushop.products', ['tedushop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('products_list', {
+            URL: "/products_list",
+            templateUrl: "/app/components/Products/productListView.html",
+            controller: "productListController"
+        }).state('product_add', {
+            URL: "/product_add",
+            templateUrl: "/app/components/Products/productAddView.html",
+            controller: "productAddController"
+        });
+    }
+})();
