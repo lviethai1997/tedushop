16dbef002e74dd9f66c416fc2d059baf5f7b9aa1
DESKTOP-2UPET6P\haile <lviethai1997@gmail.com>
2021-11-10 08:22:39 +0700

    update filter angular

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,11 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true) {
+                return 'Active'
+            } else {
+                return 'Disable'
+            }
+        }
+    })
+})(angular.module('tedushop.common'));
